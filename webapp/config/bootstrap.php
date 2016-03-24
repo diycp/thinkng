c9c67479c8b7de8d71a8a10e60ec32f0126a86ae
slince <taosikai@yeah.net>
2016-03-24 22:52:41 +0800

    更换测试项目文件目录名

---
+++ webapp/config/bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+require __DIR__ . '/../../vendor/autoload.php';
+require __DIR__ . '/../../src/start.php';
+
+date_default_timezone_set('Asia/shanghai');
--- webapp/public/index.php
+++ webapp/public/index.php
@@ -16,6 +16,4 @@ define('APP_PATH', __DIR__ . '/../application/');
 // 开启调试模式
 define('APP_DEBUG', true);
 // 加载框架引导文件
-require __DIR__ . '/../../vendor/autoload.php';
-
-require __DIR__ . '/../../src/start.php';
+include __DIR__ . '/../config/bootstrap.php';
