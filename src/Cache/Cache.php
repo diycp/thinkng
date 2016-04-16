583a30d15f7cdf9c39dce135bde686d112b3654a
hainuo <admin@hainuo.info>
2016-04-16 14:28:19 +0800

    使用psr-4规范格式化，并修复类的命名空间调用

--- src/Cache/Cache.php
+++ src/Cache/Cache.php
@@ -9,57 +9,64 @@
 // | Author: liu21st <liu21st@gmail.com>
 // +----------------------------------------------------------------------
 
-namespace Think\Cache;
+    namespace Think\Cache;
 
-/**
- * Class Cache
- * 
- * @package think
- * @method static mixed get() get(string $name)
- * @method static bool set() set(string $name, mixed $value, mixed $expire = null)
- * @method static bool rm() rm(string $name, bool $expire = false)
- * @method static bool clear() clear()
- */
-class Cache
-{
-    protected static $instance = [];
-    public static $readTimes   = 0;
-    public static $writeTimes  = 0;
+    use Think\Config\Config;
+    use Think\Log\Log;
 
     /**
-     * 操作句柄
-     * @var object
-     * @access protected
+     * Class Cache
+     *
+     * @package think
+     * @method static mixed get() get(string $name)
+     * @method static bool set() set(string $name, mixed $value, mixed $expire = null)
+     * @method static bool rm() rm(string $name, bool $expire = false)
+     * @method static bool clear() clear()
      */
-    protected static $handler = null;
-
-    /**
-     * 连接缓存
-     * @access public
-     * @param array $options  配置数组
-     * @return object
-     */
-    public static function connect(array $options = [])
+    class Cache
     {
-        $md5 = md5(serialize($options));
-        if (!isset(self::$instance[$md5])) {
-            $type  = !empty($options['type']) ? $options['type'] : 'File';
-            $class = (!empty($options['namespace']) ? $options['namespace'] : '\\Think\\cache\\driver\\') . ucwords($type);
-            unset($options['type']);
-            self::$instance[$md5] = new $class($options);
-            // 记录初始化信息
-            APP_DEBUG && Log::record('[ CACHE ] INIT ' . $type . ':' . var_export($options, true), 'info');
-        }
-        self::$handler = self::$instance[$md5];
-        return self::$handler;
-    }
+        protected static $instance = [];
+        public static $readTimes = 0;
+        public static $writeTimes = 0;
 
-    public static function __callStatic($method, $params)
-    {
-        if (is_null(self::$handler)) {
-            // 自动初始化缓存
-            self::connect(Config::get('cache'));
+        /**
+         * 操作句柄
+         *
+         * @var object
+         * @access protected
+         */
+        protected static $handler = null;
+
+        /**
+         * 连接缓存
+         *
+         * @access public
+         *
+         * @param array $options 配置数组
+         *
+         * @return object
+         */
+        public static function connect(array $options = [])
+        {
+            $md5 = md5(serialize($options));
+            if (!isset(self::$instance[$md5])) {
+                $type = !empty($options['type']) ? $options['type'] : 'File';
+                $class = (!empty($options['namespace']) ? $options['namespace'] : '\\Think\\cache\\driver\\') . ucwords($type);
+                unset($options['type']);
+                self::$instance[$md5] = new $class($options);
+                // 记录初始化信息
+                APP_DEBUG && Log::record('[ CACHE ] INIT ' . $type . ':' . var_export($options, true), 'info');
+            }
+            self::$handler = self::$instance[$md5];
+            return self::$handler;
         }
-        return call_user_func_array([self::$handler, $method], $params);
+
+        public static function __callStatic($method, $params)
+        {
+            if (is_null(self::$handler)) {
+                // 自动初始化缓存
+                self::connect(Config::get('cache'));
+            }
+            return call_user_func_array([self::$handler, $method], $params);
+        }
     }
-}
